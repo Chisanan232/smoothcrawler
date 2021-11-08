4ad68b4a5bd0b4f04b228eef24ad91cf52ed1865
Chisanan232 <chi10211201@cycu.org.tw>
2021-11-08 21:21:34 +0800

    Modify some detail about implementation.

--- smoothcrawler/data.py
+++ smoothcrawler/data.py
@@ -1,45 +1,38 @@
 from abc import ABCMeta, abstractmethod
 from http import HTTPStatus
 from typing import List, Dict, Union, Any
-# from urllib3.response import HTTPResponse
-# from requests import Response
 
 
 
 class BaseHTTPResponseParser(metaclass=ABCMeta):
 
-    _HTTPResponse = None
-
-    def __init__(self, response):
-        self._HTTPResponse = response
-
-
-    def parse_content(self) -> Any:
-        if self.get_status_code() == HTTPStatus.OK.real:
-            handled_result = self.handling_200_response()
+    def parse_content(self, response) -> Any:
+        if self.get_status_code(response=response) == HTTPStatus.OK.real:
+            handled_result = self.handling_200_response(response=response)
         else:
-            handled_result = self.handling_not_200_response()
+            handled_result = self.handling_not_200_response(response=response)
+        # Require the return value type is List, Dict or JSON, etc.
         return handled_result
 
 
     @abstractmethod
-    def get_status_code(self) -> int:
+    def get_status_code(self, response) -> int:
         pass
 
 
-    def handling_200_response(self) -> Any:
-        return self._HTTPResponse
+    def handling_200_response(self, response) -> Any:
+        return response
 
 
-    def handling_not_200_response(self) -> Any:
-        return self._HTTPResponse
+    def handling_not_200_response(self, response) -> Any:
+        return response
 
 
 
 class BaseDataHandler(metaclass=ABCMeta):
 
     @abstractmethod
-    def process(self, result):
+    def process(self, result) -> Any:
         pass
 
 
