cf8d70e6bdd577abbaa7982edba9462b7189cce5
Chisanan232 <chi10211201@cycu.org.tw>
2021-11-14 16:08:21 +0800

    Add database connections and operators modules (implementations) with ClickHouse driver.

---
+++ example/crawler_db_clickhouse.py
@@ -0,0 +1,119 @@
+from smoothcrawler.persistence.database.strategy import BaseDatabaseConnection, T
+from smoothcrawler.persistence.database import SingleConnection, ConnectionPool, DatabaseOperator
+
+from typing import Generic, Any, Tuple
+from clickhouse_driver.dbapi.connection import Connection as ClickHouseConnection
+from clickhouse_driver.dbapi.cursor import Cursor as ClickHouseCursor
+import clickhouse_driver
+
+
+
+class ClickHouseSingleConnection(SingleConnection):
+    
+    @property
+    def connection(self) -> ClickHouseConnection:
+        return super(ClickHouseSingleConnection, self).connection()
+
+
+    @property
+    def cursor(self) -> ClickHouseCursor:
+        return super(ClickHouseSingleConnection, self).cursor()
+    
+
+    def connect_database(self, **kwargs) -> ClickHouseConnection:
+        conn = clickhouse_driver.connect()
+        return conn
+
+
+    def build_cursor(self) -> ClickHouseCursor:
+        self.cursor = self.connection.cursor()
+        return self.cursor
+
+
+    def commit(self) -> None:
+        self.connection.commit()
+
+
+    def close(self) -> None:
+        if self.connection.is_closed is False:
+            if self.cursor is not None:
+                self.cursor.close()
+            self.connection.close()
+
+
+
+class ClickHouseDriverConnectionPool(ConnectionPool):
+
+    def close_pool(self) -> None:
+        pass
+
+
+    def connect_database(self, **kwargs) -> Generic[T]:
+        pass
+
+
+    def get_one_connection(self) -> Generic[T]:
+        pass
+
+
+    def build_cursor(self) -> Generic[T]:
+        pass
+
+
+    def commit(self) -> None:
+        pass
+
+
+    def close(self) -> None:
+        pass
+
+
+
+class ClickHouseOperator(DatabaseOperator):
+
+    def __init__(self, conn_strategy: BaseDatabaseConnection):
+        super(ClickHouseOperator, self).__init__(conn_strategy=conn_strategy)
+        self.__cursor: ClickHouseCursor = conn_strategy.cursor
+
+
+    @property
+    def column_names(self) -> ClickHouseCursor:
+        pass
+
+
+    @property
+    def row_count(self) -> ClickHouseCursor:
+        pass
+
+
+    def next(self) -> ClickHouseCursor:
+        pass
+
+
+    def execute(self, operator: Any, params: Tuple = None, multi: bool = False) -> ClickHouseCursor:
+        return self.__cursor.execute(operation=operator, parameters=params)
+
+
+    def execute_many(self, operator: Any, seq_params=None) -> ClickHouseCursor:
+        return self.__cursor.executemany(operation=operator, seq_of_parameters=seq_params)
+
+
+    def fetch(self) -> ClickHouseCursor:
+        pass
+
+
+    def fetch_one(self) -> ClickHouseCursor:
+        return self.__cursor.fetchone()
+
+
+    def fetch_many(self, size: int = None) -> ClickHouseCursor:
+        return self.__cursor.fetchmany(size=size)
+
+
+    def fetch_all(self) -> ClickHouseCursor:
+        return self.__cursor.fetchall()
+
+
+    def reset(self) -> None:
+        pass
+
