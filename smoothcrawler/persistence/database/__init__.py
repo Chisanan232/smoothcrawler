3d2bcda331389e89f9e17df5bfd5b8915f1e73b9
Chisanan232 <chi10211201@cycu.org.tw>
2022-01-16 22:01:10 +0800

    Delete unused modules and testing code.

--- smoothcrawler/persistence/database/__init__.py
+++
@@ -1,6 +0,0 @@
-from smoothcrawler.persistence.database.strategy import (
-    BaseDatabaseConnection as _BaseDataBaseConnection,
-    SingleConnection, ConnectionPool)
-from smoothcrawler.persistence.database.operator import DatabaseOperator
-from smoothcrawler.persistence.database.layer import CrawlerDao
-
