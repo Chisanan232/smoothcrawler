ad617098f5810f19d991dd333c71fd6319cd8a1b
Chisanan232 <chi10211201@cycu.org.tw>
2022-01-19 16:00:58 +0800

    1. Add new object 'AsyncHTTPResponseParser' for AsyncSimpleCrawler. 2. Add new object 'AsyncDataHandler' for AsyncSimpleCrawler.

--- smoothcrawler/components/data.py
+++ smoothcrawler/components/data.py
@@ -30,11 +30,44 @@ class BaseHTTPResponseParser(metaclass=ABCMeta):
         return response
 
 
+class BaseAsyncHTTPResponseParser(BaseHTTPResponseParser):
+
+    async def parse_content(self, response) -> Generic[T]:
+        _http_resp_status = await self.get_status_code(response=response)
+        if _http_resp_status == HTTPStatus.OK.real:
+            handled_result = await self.handling_200_response(response=response)
+        else:
+            handled_result = await self.handling_not_200_response(response=response)
+        # Require the return value type is List, Dict or JSON, etc.
+        return handled_result
+
+
+    @abstractmethod
+    async def get_status_code(self, response) -> int:
+        pass
+
+
+    async def handling_200_response(self, response) -> Generic[T]:
+        return response
+
+
+    async def handling_not_200_response(self, response) -> Generic[T]:
+        return response
+
+
 
 class BaseDataHandler(metaclass=ABCMeta):
 
     @abstractmethod
     def process(self, result) -> Generic[T]:
+        pass
+
+
+
+class BaseAsyncDataHandler(metaclass=ABCMeta):
+
+    @abstractmethod
+    async def process(self, result) -> Generic[T]:
         pass
 
 
