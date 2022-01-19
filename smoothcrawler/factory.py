18f5235189dff8b6a8c428decf32f547843142ed
Chisanan232 <chi10211201@cycu.org.tw>
2022-01-19 15:59:54 +0800

    Add new factory object 'AsyncCrawlerFactory' for AsyncSimpleCrawler.

--- smoothcrawler/factory.py
+++ smoothcrawler/factory.py
@@ -1,8 +1,10 @@
 from abc import ABCMeta, abstractmethod
 from typing import Any, TypeVar, Generic
 
-from .components.httpio import BaseHTTP
-from .components.data import BaseHTTPResponseParser, BaseDataHandler
+from .components.httpio import HTTP, AsyncHTTP
+from .components.data import (
+    BaseHTTPResponseParser, BaseDataHandler,
+    BaseAsyncHTTPResponseParser, BaseAsyncDataHandler)
 from .persistence import PersistenceFacade
 
 
@@ -39,19 +41,19 @@ class BaseFactory(metaclass=ABCMeta):
 class CrawlerFactory(BaseFactory):
 
     def __init__(self):
-        self.__http_factory: BaseHTTP = None
+        self.__http_factory: HTTP = None
         self.__response_parser_factory: BaseHTTPResponseParser = None
         self.__data_handling_factory: BaseDataHandler = None
         self.__persistence_factory: PersistenceFacade = None
 
 
     @property
-    def http_factory(self) -> BaseHTTP:
+    def http_factory(self) -> HTTP:
         return self.__http_factory
 
 
     @http_factory.setter
-    def http_factory(self, factory: BaseHTTP) -> None:
+    def http_factory(self, factory: HTTP) -> None:
         self.__http_factory = factory
 
 
@@ -72,6 +74,56 @@ class CrawlerFactory(BaseFactory):
 
     @data_handling_factory.setter
     def data_handling_factory(self, factory: BaseDataHandler) -> None:
+        self.__data_handling_factory = factory
+
+
+    @property
+    def persistence_factory(self) -> PersistenceFacade:
+        return self.__persistence_factory
+
+
+    @persistence_factory.setter
+    def persistence_factory(self, factory: PersistenceFacade) -> None:
+        self.__persistence_factory = factory
+
+
+
+class AsyncCrawlerFactory(BaseFactory):
+
+    def __init__(self):
+        self.__http_factory: AsyncHTTP = None
+        self.__response_parser_factory: BaseAsyncHTTPResponseParser = None
+        self.__data_handling_factory: BaseAsyncDataHandler = None
+        self.__persistence_factory: PersistenceFacade = None
+
+
+    @property
+    def http_factory(self) -> AsyncHTTP:
+        return self.__http_factory
+
+
+    @http_factory.setter
+    def http_factory(self, factory: AsyncHTTP) -> None:
+        self.__http_factory = factory
+
+
+    @property
+    def parser_factory(self) -> BaseAsyncHTTPResponseParser:
+        return self.__response_parser_factory
+
+
+    @parser_factory.setter
+    def parser_factory(self, factory: BaseAsyncHTTPResponseParser) -> None:
+        self.__response_parser_factory = factory
+
+
+    @property
+    def data_handling_factory(self) -> BaseAsyncDataHandler:
+        return self.__data_handling_factory
+
+
+    @data_handling_factory.setter
+    def data_handling_factory(self, factory: BaseAsyncDataHandler) -> None:
         self.__data_handling_factory = factory
 
 
