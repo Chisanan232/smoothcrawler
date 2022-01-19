5c9906d44a15cdb74c58dacc333bb8d60450bef0
Chisanan232 <chi10211201@cycu.org.tw>
2022-01-19 08:05:08 +0800

    Modify the import-path of modules in components.

--- smoothcrawler/__init__.py
+++ smoothcrawler/__init__.py
@@ -1,4 +1,4 @@
 from smoothcrawler.urls import URL
-from smoothcrawler.httpio import HTTP, AsyncHTTP
-from smoothcrawler.data import BaseHTTPResponseParser, BaseDataHandler
+from smoothcrawler.components.httpio import HTTP, AsyncHTTP
+from smoothcrawler.components.data import BaseHTTPResponseParser, BaseDataHandler
 from smoothcrawler.crawler import SimpleCrawler, ExecutorCrawler, AsyncSimpleCrawler, PoolCrawler
