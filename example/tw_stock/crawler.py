3d80406a878ce58deed82412de259d2c0d63a609
Chisanan232 <chi10211201@cycu.org.tw>
2022-06-01 07:31:32 +0800

    [Style] (example) Modify the code style of examples. [skip ci]

--- example/tw_stock/crawler.py
+++ example/tw_stock/crawler.py
@@ -32,33 +32,32 @@ class StockCrawlerImpl:
 
         # Crawler Role: Simple Crawler
         sc = SimpleCrawler(factory=self._cf)
-
         data = sc.run("GET", Test_URL_TW_Stock)
         print(f"[DEBUG] data: {data}")
 
 
     def run_as_async_simple_crawler(self):
-        # Crawler Role: Asynchronous Simple Crawler
-        sc = AsyncSimpleCrawler(factory=self._acf, executors=2)
-
+        # Generate URLs
         url = URL(base=Test_URL_TW_Stock_With_Option, start="20210801", end="20211001", formatter="yyyymmdd")
         url.set_period(days=31, hours=0, minutes=0, seconds=0)
         target_urls = url.generate()
         print(f"Target URLs: {target_urls}")
 
+        # Crawler Role: Asynchronous Simple Crawler
+        sc = AsyncSimpleCrawler(factory=self._acf, executors=2)
         data = sc.run("GET", target_urls)
         print(f"[DEBUG] data: {data}")
 
 
     def run_as_executor_crawler(self):
-        # Crawler Role: Executor Crawler
-        sc = ExecutorCrawler(factory=self._cf, mode=RunAsParallel, executors=3)
-
+        # Generate URLs
         url = URL(base=Test_URL_TW_Stock_With_Option, start="20210801", end="20211001", formatter="yyyymmdd")
         url.set_period(days=31, hours=0, minutes=0, seconds=0)
         target_urls = url.generate()
         print(f"Target URLs: {target_urls}")
 
+        # Crawler Role: Executor Crawler
+        sc = ExecutorCrawler(factory=self._cf, mode=RunAsParallel, executors=3)
         data = sc.run(method="GET", url=target_urls, lock=False, sema_value=3)
         print(f"[DEBUG] data: {data}")
         for d in data:
@@ -70,10 +69,16 @@ class StockCrawlerImpl:
 
 
     def run_as_pool_crawler(self):
+        # Generate URLs
+        url = URL(base=Test_URL_TW_Stock_With_Option, start="20210801", end="20211001", formatter="yyyymmdd")
+        url.set_period(days=31, hours=0, minutes=0, seconds=0)
+        target_urls = url.generate()
+        print(f"Target URLs: {target_urls}")
+
         # # Crawler Role: Pool Crawler
         with PoolCrawler(factory=self._cf, mode=RunAsParallel, pool_size=5) as pc:
-            pc.init(lock=False, sema_value=3)
-            data = pc.async_apply(method="GET", urls=[Test_URL_TW_Stock])
+            # pc.init(lock=False, sema_value=3)
+            data = pc.async_apply(method="GET", urls=target_urls)
             print(f"[DEBUG] data: {data}")
             for d in data:
                 print(f"[DEBUG] data: {d.data}")
