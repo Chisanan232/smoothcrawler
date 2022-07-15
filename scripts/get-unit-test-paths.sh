4a5aaa90019879531d97d75bf2186efbb3660f0d
Chisanan232 <chi10211201@cycu.org.tw>
2022-07-15 10:06:21 +0800

    [Breaking Change] (script) Rename the script file name about getting all unit test items.

--- scripts/get-unit-test-paths.sh
+++ scripts/get-unit-test-paths.sh
@@ -5,6 +5,7 @@ runtime_os=$1
 
 declare -a init_tests
 declare -a components_tests
+declare -a appintegration_tests
 
 getalltests() {
     declare -a testpatharray=( $(ls -F $1 | grep -v '/$' | grep -v '__init__.py' | grep -v 'test_config.py' | grep -v -E '^_[a-z_]{1,64}.py' | grep -v '__pycache__'))
@@ -17,18 +18,23 @@ getalltests() {
     if echo $1 | grep -q "components";
     then
         components_tests=${alltestpaths[@]}
+    elif echo $1 | grep -q "appintegration";
+    then
+        appintegration_tests=${alltestpaths[@]}
     else
         init_tests=${alltestpaths[@]}
     fi
 }
 
-init_path=test/
-componentspath=test/components/
+init_path=test/unit_test/
+components_path=test/unit_test/components/
+appintegration_path=test/unit_test/appintegration/
 
 getalltests $init_path
-getalltests $componentspath
+getalltests $components_path
+getalltests $appintegration_path
 
-dest=( "${init_tests[@]} ${components_tests[@]}" )
+dest=( "${init_tests[@]} ${components_tests[@]} ${appintegration_tests[@]}" )
 
 
 if echo $runtime_os | grep -q "windows";
